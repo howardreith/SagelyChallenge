27b950bcecfe4ce5360a7950619dd29effbb75a1
howardreith <howard.reith@gmail.com>
2022-08-15 09:04:10 -0400

    generate html from webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      template: 'src/index.html',
+      filename: './index.html',
+    }),
+  ],
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+    ],
   },
 };
